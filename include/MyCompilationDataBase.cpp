ab2156a64c21bb20a2de5dcdbb14fd4dae67e462
reed <1457882960@qq.com>
2026-03-16 10:37:45 +0800

    通过LLVM_DIR访问clang而不是硬编码绝对路径

--- include/MyCompilationDataBase.cpp
+++ include/MyCompilationDataBase.cpp
@@ -6,20 +6,34 @@ using Self = MyCompilationDataBase;
 
 namespace fs = std::filesystem;
 
-std::vector<clang::tooling::CompileCommand> Self::getCompileCommands(llvm::StringRef FilePath) const {
-    return {clang::tooling::CompileCommand(
-        fs::path(FilePath.str()).parent_path().string(),
-        FilePath.str(),
-        {
-            "/opt/llvm-tooling/bin/clang++",
-            // "-E",
-            // "-v",
-            "-resource-dir=/opt/llvm-tooling/lib/clang/22",
-            "-I/home/reed/.local/include",
-            "-std=c++23",
-            "-DREED",
+const char* llvm_dir = getenv("LLVM_DIR");
+const char* home = getenv("HOME");
+
+std::vector<clang::tooling::CompileCommand> Self::getCompileCommands(llvm::StringRef FilePath) const
+{
+    fs::path clang_path = fs::path(llvm_dir) / "bin" / "clang++";
+    fs::path clang_lib_path = fs::path(clang_path) / "lib" / "clang";
+    fs::path clang_resource_dir;
+    for (auto it = fs::directory_iterator(clang_lib_path), e = fs::directory_iterator(); it != e; ++it)
+    {
+        clang_resource_dir = *it;
+    }
+    fs::path local_include_dir = fs::path(home) / ".local" / "include";
+    return {
+        clang::tooling::CompileCommand(
+            fs::path(FilePath.str()).parent_path().string(),
+            FilePath.str(),
+            {
+                clang_path.string(),
+                "-E",
+                "-v",
+                "-resource-dir=" + clang_resource_dir.string(),
+                "-I" + local_include_dir.string(),
+                "-std=c++23",
+                "-DREED",
+                FilePath.str()
+            },
             FilePath.str()
-        },
-        FilePath.str()
-    )};
+        )
+    };
 }
